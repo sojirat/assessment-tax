009dcec6c549eb5042f7bc45649179b864f7ae61
Milk Sojirat <zsojirat.s@pttdigital.com>
2024-04-25 22:56:45 +0700

    [Add] : calculate tax with tax level detail

--- tax/calculations_test.go
+++ tax/calculations_test.go
@@ -1,6 +1,8 @@
 package tax_test
 
 import (
+	"reflect"
+	"sort"
 	"testing"
 
 	"github.com/sojirat/assessment-tax/tax"
@@ -12,7 +14,7 @@ func TestCalculate(t *testing.T) {
 		totalIncome float64
 		wht         float64
 		allowances  []tax.Allowance
-		expected    float64
+		expected    tax.TaxCalculationResponse
 	}{
 		{
 			name:        "Basic Test 1",
@@ -24,15 +26,50 @@ func TestCalculate(t *testing.T) {
 					Amount:        100000.0,
 				},
 			},
-			expected: 19000.0,
+			expected: tax.TaxCalculationResponse{
+				Tax: 19000.0,
+				TaxLevel: []tax.LevelCalculationResponse{
+					{
+						Level: "0-150,000",
+						Tax:   0.0,
+					},
+					{
+						Level: "150,001-500,000",
+						Tax:   19000.0,
+					},
+					{
+						Level: "500,001-1,000,000",
+						Tax:   0.0,
+					},
+					{
+						Level: "1,000,001-2,000,000",
+						Tax:   0.0,
+					},
+					{
+						Level: "2,000,001 ขึ้นไป",
+						Tax:   0.0,
+					},
+				},
+			},
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			if actual := tax.CalculateTax(tc.totalIncome, tc.wht, tc.allowances); actual != tc.expected {
-				t.Errorf("Expected %f, got %f", tc.expected, actual)
+			actual := tax.CalculateTax(tc.totalIncome, tc.wht, tc.allowances)
+
+			sort.Slice(actual.TaxLevel, func(i, j int) bool {
+				return actual.TaxLevel[i].Level < actual.TaxLevel[j].Level
+			})
+
+			sort.Slice(tc.expected.TaxLevel, func(i, j int) bool {
+				return tc.expected.TaxLevel[i].Level < tc.expected.TaxLevel[j].Level
+			})
+
+			if !reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Expected %v, got %v", tc.expected, actual)
 			}
 		})
 	}
+
 }

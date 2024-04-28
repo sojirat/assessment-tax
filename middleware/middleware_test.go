8226ed4a55e4e05f92289377aaf58d1bdc3d5bb8
Milk Sojirat <zsojirat.s@pttdigital.com>
2024-04-28 16:23:54 +0700

    [Update] : TestAuthMiddleware

--- middleware/middleware_test.go
+++ middleware/middleware_test.go
@@ -3,6 +3,8 @@ package middleware_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/labstack/echo/v4"
@@ -11,99 +13,50 @@ import (
 )
 
 func TestAuthMiddleware(t *testing.T) {
+	url := "localhost:8080/admin/deductions/k-receipt"
+
 	tests := []struct {
 		name         string
 		req          *http.Request
 		expectedCode int
-		expectedBody string
 	}{
 		{
 			name:         "No credentials provided",
-			req:          httptest.NewRequest(http.MethodGet, "/", nil),
+			req:          httptest.NewRequest(http.MethodPatch, url, nil),
 			expectedCode: http.StatusUnauthorized,
-			expectedBody: `{"message":"Unauthorized"}`,
 		},
 		{
 			name:         "Incorrect credentials provided",
-			req:          httptest.NewRequest(http.MethodGet, "/", nil),
+			req:          httptest.NewRequest(http.MethodPatch, url, nil),
 			expectedCode: http.StatusUnauthorized,
-			expectedBody: `{"message":"Unauthorized"}`,
 		},
 		{
 			name:         "Correct credentials provided",
-			req:          httptest.NewRequest(http.MethodGet, "/", nil),
+			req:          httptest.NewRequest(http.MethodPatch, url, strings.NewReader(`{"amount": 50000}`)),
 			expectedCode: http.StatusOK,
-			expectedBody: "",
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			c := echo.New().NewContext(tc.req, rec)
+
 			if tc.name == "Incorrect credentials provided" {
 				tc.req.SetBasicAuth("incorrect", "credentials")
 			} else if tc.name == "Correct credentials provided" {
 				tc.req.SetBasicAuth("adminTax", "admin!")
 			}
 
-			rec := httptest.NewRecorder()
-			c := echo.New().NewContext(tc.req, rec)
+			os.Setenv("ADMIN_USERNAME", "adminTax")
+			os.Setenv("ADMIN_PASSWORD", "admin!")
 
 			err := middleware.AuthMiddleware(func(c echo.Context) error {
 				return nil
 			})(c)
 
 			assert.Equal(t, tc.expectedCode, rec.Code)
-			assert.JSONEq(t, tc.expectedBody, rec.Body.String())
 			assert.NoError(t, err)
 		})
 	}
 }
-
-// func TestAuthMiddleware(t *testing.T) {
-// 	// Set environment variables for testing
-// 	os.Setenv("ADMIN_USERNAME", "adminTax")
-// 	os.Setenv("ADMIN_PASSWORD", "admin!")
-
-// 	// Create a new echo instance
-// 	e := echo.New()
-
-// 	// Create a new request with no credentials
-// 	reqNoCreds := httptest.NewRequest(http.MethodGet, "/", nil)
-// 	recNoCreds := httptest.NewRecorder()
-// 	cNoCreds := e.NewContext(reqNoCreds, recNoCreds)
-
-// 	// Test case: No credentials provided
-// 	err := middleware.AuthMiddleware(func(c echo.Context) error {
-// 		return nil
-// 	})(cNoCreds)
-// 	assert.Equal(t, http.StatusUnauthorized, recNoCreds.Code)
-// 	assert.JSONEq(t, `{"message":"Unauthorized"}`, recNoCreds.Body.String())
-// 	assert.NoError(t, err)
-
-// 	// Create a new request with incorrect credentials
-// 	reqIncorrectCreds := httptest.NewRequest(http.MethodGet, "/", nil)
-// 	reqIncorrectCreds.SetBasicAuth("incorrect", "credentials")
-// 	recIncorrectCreds := httptest.NewRecorder()
-// 	cIncorrectCreds := e.NewContext(reqIncorrectCreds, recIncorrectCreds)
-
-// 	// Test case: Incorrect credentials provided
-// 	err = middleware.AuthMiddleware(func(c echo.Context) error {
-// 		return nil
-// 	})(cIncorrectCreds)
-// 	assert.Equal(t, http.StatusUnauthorized, recIncorrectCreds.Code)
-// 	assert.JSONEq(t, `{"message":"Unauthorized"}`, recIncorrectCreds.Body.String())
-// 	assert.NoError(t, err)
-
-// 	// Create a new request with correct credentials
-// 	reqCorrectCreds := httptest.NewRequest(http.MethodGet, "/", nil)
-// 	reqCorrectCreds.SetBasicAuth("adminTax", "admin!")
-// 	recCorrectCreds := httptest.NewRecorder()
-// 	cCorrectCreds := e.NewContext(reqCorrectCreds, recCorrectCreds)
-
-// 	// Test case: Correct credentials provided
-// 	err = middleware.AuthMiddleware(func(c echo.Context) error {
-// 		return nil
-// 	})(cCorrectCreds)
-// 	assert.Equal(t, http.StatusOK, recCorrectCreds.Code)
-// 	assert.NoError(t, err)
-// }
